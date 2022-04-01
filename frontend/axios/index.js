9d0419c5b2edaab1a0f8db1dcfac45f662a00760
William Lee <leewp8@gmail.com>
2022-04-01 14:11:09 -0400

    initial commit

--- frontend/axios/index.js
+++ frontend/axios/index.js
@@ -1,1 +1,15 @@
 // ✨ implement axiosWithAuth
+
+//not 100% sure but pretty sure this is reusable throughout
+
+import axios from 'axios'
+
+export const axiosWithAuth = () => {
+    const token = localStorage.getItem("token");
+    return axios.create({
+        baseURL: "http://localhost:9000/api",
+        headers: {
+            authorization: token,
+        },
+    });
+};
